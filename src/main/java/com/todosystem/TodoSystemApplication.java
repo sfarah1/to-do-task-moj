cccedcb768927c5c445ce09c7323268fa1516c2f
sfarah1 <sadieq.ai.f@gmail.com>
2025-09-20 17:14:06 +0100

    created base packages (model, repository, service, controller, dto)

---
+++ src/main/java/com/todosystem/TodoSystemApplication.java
@@ -0,0 +1,11 @@
+package com.todosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TodoSystemApplication.class, args);
+    }
+}
