f6398864b896f14cd183e9ebb4f1394805f2ab28
sfarah1 <sadieq.ai.f@gmail.com>
2025-09-20 17:17:34 +0100

    added task repository

---
+++ src/main/java/com/todosystem/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.todosystem.repository;
+
+import com.todosystem.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface TaskRepository extends JpaRepository<Task, UUID> {}
